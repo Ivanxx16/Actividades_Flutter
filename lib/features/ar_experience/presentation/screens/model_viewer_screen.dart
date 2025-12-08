1753bc5a5df1f5c5d797a6482e4df9938694fdaf
Ivanxx16 <sanchezanthony842@gmail.com>
2025-12-08 18:38:41 -0500

    Astronauta y texto

--- lib/features/ar_experience/presentation/screens/model_viewer_screen.dart
+++ lib/features/ar_experience/presentation/screens/model_viewer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class ModelViewerScreen extends StatelessWidget {
   const ModelViewerScreen({super.key});
@@ -8,9 +9,14 @@ class ModelViewerScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Model Viewer (Web/Android/iOS)')),
       body: const Center(
-        child: Text(
-          'Aquí implementaremos el Model Viewer',
-          style: TextStyle(fontSize: 18, color: Colors.grey),
+        child: ModelViewer(
+          src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
+          backgroundColor: Color.fromARGB(255, 234, 234, 239),
+          cameraControls: true,
+          autoRotate: true,
+          //Habilitar el boton para ver el modelo
+          ar: true,
+          alt: 'AQUI UN TEXTO ALTERNATIVO SI NO EXISTE EL ARCHIVO'
         ),
       ),
     );
